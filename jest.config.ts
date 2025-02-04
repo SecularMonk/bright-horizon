322900ec02aab08adb7b84b1b725fb42148bb672
Calum Craig <ccraig@itzamedia.com>
2025-02-04 21:45:33 +0000

    Update config & packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-import type { Config } from 'jest'
+import type { Config } from 'jest';
 
 const config: Config = {
+   preset: 'ts-jest',
    verbose: true,
-}
+   transform: {
+      '^.+\\.(ts|tsx)?$': 'ts-jest',
+   },
+};
 
-export default config
+export default config;
