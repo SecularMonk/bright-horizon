580f6dd6fa7fe92066aba7bc9665b8075e975f2b
Calum Craig <ccraig@itzamedia.com>
2025-02-05 14:57:31 +0000

    Add winston & update logging

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { queryJobsAPI, queryMembersAPI } from './api/recruitment';
+import { logger } from './logging';
 import { generateMatches } from './recommendations/match';
 
 console.log('Hello, world!');
 
 async function main() {
+   logger.info('Beginning job search...');
+
    const [members, jobs] = await Promise.all([
       queryMembersAPI(),
       queryJobsAPI(),
-   ]);
+   ]).catch((err) => {
+      logger.error(`Failed to query job and member data ${err}`);
+      throw err;
+   });
+   logger.info(`Successfully queries job and member data`);
 
    generateMatches(jobs, members);
+   logger.info(`Successfully completed job match`);
 }
 
 main();
