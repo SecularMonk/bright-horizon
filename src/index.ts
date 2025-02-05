04f03555fdbf3372bd1930cd39cbf696568609eb
Calum Craig <ccraig@itzamedia.com>
2025-02-05 11:30:09 +0000

    Add early match generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!')
+import { queryJobsAPI, queryMembersAPI } from './api/recruitment';
+import { generateMatches } from './recommendations/match';
+
+console.log('Hello, world!');
+
+async function main() {
+   const members = await queryMembersAPI();
+   console.log('members in main: ', members);
+
+   const jobs = await queryJobsAPI();
+   console.log('jobs in main: ', jobs);
+
+   generateMatches(jobs, members);
+}
+
+main();
